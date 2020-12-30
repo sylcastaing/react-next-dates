846c63580c2bd47e5f376fd44ae1045bd6279dbe
Sylvain Castaing <castaing.sylvain@gmail.com>
2020-12-30 14:11:02 +0100

    [build] add style.scss copy to dist folder

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,6 +1,7 @@
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const copy = require('rollup-plugin-copy');
 
 module.exports = {
   rollup(config, options) {
@@ -15,8 +16,20 @@ module.exports = {
         inject: false,
         // only write out CSS for the first bundle (avoids pointless extra files):
         extract: !!options.writeMeta ? 'style.css' : false,
-      })
+      }),
     );
+
+    config.plugins.push(
+      copy({
+        targets: [
+          {
+            src: 'src/style.scss',
+            dest: 'dist',
+          },
+        ],
+      }),
+    );
+
     return config;
   },
 };
